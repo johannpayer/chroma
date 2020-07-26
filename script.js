fb50ca8069e593dbe85af3759a6956e6a151cd14
flamesdev <johannpayer123@gmail.com>
2020-07-26 03:33:37 -0400

    Cleaned code

--- script.js
+++ script.js
@@ -4,43 +4,32 @@ data.forEach(x => colors.push({
   color: hexToRgb(x.Color),
 }));
 
-let headerElement;
-let hexElement;
-let dataElement;
-$(function () {
-  headerElement = document.getElementById("header");
-  hexElement = document.getElementById("hex");
-  dataElement = document.getElementById("data");
-});
-
 document.addEventListener("keydown", function (event) {
   let key = String.fromCharCode(event.keyCode).toLowerCase();
   if ("0123456789abcdef".includes(key)) {
-    if (hexElement.innerHTML.length < 7) {
-      hexElement.innerHTML += key;
-      if (hexElement.innerHTML.length == 7)
-        updateData(hexToRgb(hexElement.innerHTML.substring(1, hexElement.innerHTML.length)));
+    if (hex.innerHTML.length < 7) {
+      hex.innerHTML += key;
+      if (hex.innerHTML.length === 7) {
+        updateData(hexToRgb(hex.innerHTML.substring(1, hex.innerHTML.length)));
+      }
     }
   } else if (event.keyCode === 8) {
-    if (hexElement.innerHTML.length > 1) {
-      hexElement.innerHTML =
-        hexElement.innerHTML.substring(0, hexElement.innerHTML.length - 1);
-      if (hexElement.innerHTML.length === 7)
-        dataElement.style.visibility = "hidden";
+    if (hex.innerHTML.length > 1) {
+      hex.innerHTML = hex.innerHTML.substring(0, hex.innerHTML.length - 1);
+      if (hex.innerHTML.length === 7) {
+        colorData.style.visibility = "hidden";
+      }
     }
   }
 });
 
 function updateData(color) {
   let brightness = getLuminosity(color);
-  Array.prototype.forEach.call([header, hexElement, dataElement], i => {
-    i.style.color = brightness <= 0.5 ? "white" : "black";
-  });
-  dataElement.style.visibility = "visible";
-  dataElement.innerHTML = getClosestColor(color).name + "<br>RGB: " +
-    color[0] + ", " + color[1] + ", " + color[2] + "<br>Brightness: " +
-    Math.round(brightness * 100) + "%<br>Opposite: " + rgbToHex(getOpposite(color));
-  document.body.style.backgroundColor = hexElement.innerHTML;
+  [header, hex, colorData].forEach(x => x.style.color = brightness <= 0.5 ? "white" : "black");
+  colorData.style.visibility = "visible";
+  colorData.innerHTML = getClosestColor(color).name + "<br>RGB: " + color[0] + ", " + color[1] + ", " + color[2] + "<br>Brightness: " + Math.round(brightness * 100) + "%<br>Opposite: " +
+    rgbToHex(getOpposite(color));
+  document.body.style.backgroundColor = hex.innerHTML;
 }
 
 function getOpposite(color) {
@@ -74,7 +63,7 @@ function getClosestColor(color) {
   let closestColor;
   colors.forEach(x => {
     let similarity = Math.abs(color[0] - x.color[0]) + Math.abs(color[1] - x.color[1]) + Math.abs(color[2] - x.color[2]);
-    if (closest == null || similarity < closest) {
+    if (closest === null || similarity < closest) {
       closest = similarity;
       closestColor = x;
     }
